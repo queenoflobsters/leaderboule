195bfef3ccce34836578616b6d1410bb56ce2819
gabribribribri <fuelanemo149@gmail.com>
2026-08-15 21:00:12 +0200

    [leaderboard] fixed very ugly syntax

--- src/client/leaderboard.rs
+++ src/client/leaderboard.rs
@@ -1,56 +1,67 @@
 use dioxus::prelude::*;
 use crate::api::db::global::{self, LeaderboardUserCard};
 
-
 const LEADERBOARD_CSS: Asset = asset!("assets/leaderboard.css");
 const RELOAD_SVG: Asset = asset!("assets/reload.svg");
 
 #[component]
 pub fn Leaderboard() -> Element {
-    // Change page and page size
-    let user_perfs_hook = use_server_future(|| global::get_leaderboard_cards(0, 5));
-    let mut reload_hook = user_perfs_hook.clone();
-    rsx! {
-        document::Stylesheet { href : LEADERBOARD_CSS }
+    let mut refresh_count = use_signal(|| 0);
+    let page = use_signal(|| 0);
+    let page_size = use_signal(|| 5);
 
-        div {
-            class: "leaderboard-container",
-            div {
-                class: "banner",
-                Banner { on_click: move |_| {
-                        if let Ok(resource) = &mut reload_hook {
-                            resource.restart();
-                        }
-                    } }
+    rsx! {
+        document::Stylesheet { href: LEADERBOARD_CSS }
+
+        div { class: "leaderboard-container",
+            div { class: "banner",
+                Banner { on_reload: move |_| refresh_count += 1 }
             }
-            div {
-                class: "cards",
+            div { class: "cards",
                 SuspenseBoundary {
-                    fallback: move |_| rsx! { p { class:"abnormal-state-message", "Patience..." } },
-                    PerfCardsManager { user_perfs_hook }
+                    fallback: |_| rsx! { p { class: "abnormal-state-message", "Patience..." } },
+                    // 2. The suspended component receives clean signals
+                    PerfCardsList { refresh_count, page, page_size }
                 }
             }
         }
-
     }
 }
 
 #[component]
-fn PerfCardsManager(
-    user_perfs_hook: Result<Resource<Result<Vec<LeaderboardUserCard>, ServerFnError>>, RenderError>,
+fn PerfCardsList(
+    refresh_count: Signal<usize>,
+    page: Signal<u64>,
+    page_size: Signal<u64>,
 ) -> Element {
-    match user_perfs_hook?() {
-        Some(Ok(user_perfs)) => rsx! {PerfCards { user_perfs }},
-        Some(Err(err)) => rsx! {p { class:"pending-messages", "Oulah... Erreur\n{err}" }},
-        None => rsx! {p { class:"pending-messages", "Patience..." }}    }
+    let cards_resource = use_server_future(move || {
+        let _ = refresh_count(); // Triggered on reload button click
+        let p = page();
+        let size = page_size();
+        async move { global::get_leaderboard_cards(p, size).await }
+    })?;
+
+    match cards_resource() {
+        Some(Ok(user_perfs)) => rsx! {
+            for (i, user) in user_perfs.iter().enumerate() {
+                CardItem { index: i, user: user.clone() }
+            }
+        },
+        Some(Err(err)) => rsx! {
+            p { class: "pending-messages", "Oulah... Erreur\n{err}" }
+        },
+        None => rsx! {
+            p { class: "pending-messages", "Patience..." }
+        }
+    }
 }
 
 #[component]
-fn Banner(on_click: EventHandler<MouseEvent>) -> Element {
+fn Banner(on_reload: EventHandler<MouseEvent>) -> Element {
     rsx! {
         button {
             class: "reload-button",
-            onclick: move |evt| on_click.call(evt),
+            onclick: move |evt| on_reload.call(evt),
             img {
                 class: "reload-icon",
                 src: RELOAD_SVG,
@@ -63,35 +74,29 @@ fn Banner(on_click: EventHandler<MouseEvent>) -> Element {
 }
 
 #[component]
-fn PerfCards(user_perfs: Vec<LeaderboardUserCard>) -> Element {
+fn CardItem(index: usize, user: LeaderboardUserCard) -> Element {
+    let games_lost = user.games_played.saturating_sub(user.games_won);
+    let ratio = (100 * user.games_won).checked_div(user.games_played).unwrap_or(0);
+    let podium_class = match index {
+        0 => "first",
+        1 => "second",
+        2 => "third",
+        _ => "",
+    };
+
     rsx! {
-        for (i, user) in user_perfs.iter().enumerate() {
-            {
-                let games_lost = user.games_played - user.games_won;
-                let ratio = (100*user.games_won).checked_div(user.games_played).unwrap_or(0);
-                let podium_class = match i {
-                    0 => "first",
-                    1 => "second",
-                    2 => "third",
-                    _ => ""
-                };
-                rsx! {
-                    div {
-                        class: "card {podium_class}",
-                        div { class: "top-left", "{user.username}" }
-                        div { class: "top-right", "{user.elo}" }
-                        div { class: "bottom-left",
-                            "Total:"
-                            span { class: "card-digits", "{user.games_played}"}
-                            "Gagnées:"
-                            span { class: "card-digits", "{user.games_won}"}
-                            "Perdues:"
-                            span { class: "card-digits", "{games_lost}"}
-                        }
-                        div { class: "bottom-right", "{ratio}%"}
-                    }
-                }
+        div { class: "card {podium_class}",
+            div { class: "top-left", "{user.username}" }
+            div { class: "top-right", "{user.elo}" }
+            div { class: "bottom-left",
+                "Total:"
+                span { class: "card-digits", "{user.games_played}" }
+                "Gagnées:"
+                span { class: "card-digits", "{user.games_won}" }
+                "Perdues:"
+                span { class: "card-digits", "{games_lost}" }
             }
+            div { class: "bottom-right", "{ratio}%" }
         }
     }
 }
