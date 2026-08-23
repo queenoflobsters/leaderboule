1a06082259e3486e38b86765dbbbb2c5eb6f6bf7
gabribribribri <fuelanemo149@gmail.com>
2026-08-23 03:32:34 +0200

    [not_found] added funny little page_not_found

--- src/client/page_not_found.rs
+++ src/client/page_not_found.rs
@@ -1,8 +1,19 @@
 use dioxus::prelude::*;
 
+use crate::api::utils;
+
 #[component]
 pub fn PageNotFound(segments: Vec<String>) -> Element {
+    let mut filler = use_signal(String::new);
+    use_future(move || async move {
+        loop {
+            filler.write().push_str("404 ");
+            utils::sleep_ms(30).await;
+        }
+    });
     rsx! {
-        p { "TODO FAIRE CETTE PAGE" }
+        h1 { "404 Page non trouvée" }
+        h3 { "Désolé" }
+        p { "{filler}" }
     }
 }
